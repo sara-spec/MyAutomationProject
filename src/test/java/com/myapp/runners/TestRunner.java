e0341e412210246fdaf6d5e81889a75ccc99679b
sara-spec <zuhal.kamaci1@gmail.com>
2020-12-23 23:37:59 -0500

    after plugin

--- src/test/java/com/myapp/runners/TestRunner.java
+++ src/test/java/com/myapp/runners/TestRunner.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
+        plugin = {
+              "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/myapp/step_definitions",
         dryRun = false
